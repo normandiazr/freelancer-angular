d779b49e87ebf3900a1c6462c19f8159a1031c2f
Norman <bytesoft70@gmail.com>
2020-06-08 08:23:30 +0200

    plantilla adaptada paso 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarModule } from './components/navbar/navbar.module';
+import { HeaderModule } from './components/header/header.module';
+import { PortfolioModule } from './components/portfolio/portfolio.module';
+import { ContactModule } from './components/contact/contact.module';
+import { FooterModule } from './components/footer/footer.module';
+import { AboutModule } from './components/about/about.module';
+import { CopyrightModule } from './components/copyright/copyright.module';
 
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NavbarModule,
+    HeaderModule,
+    PortfolioModule,
+    ContactModule,
+    FooterModule,
+    AboutModule,
+    CopyrightModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
